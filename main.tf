e81ff54288bad6d6afd73e82a755b8117742b1cb
Mark J Menger <mark.j.menger@gmail.com>
2022-04-05 00:19:05 +0000

    remove unnecessary config

--- main.tf
+++ main.tf
@@ -100,10 +100,6 @@ resource null_resource cis_init {
     inline = [
       "sleep 10",
       "kubectl create secret generic bigip-login -n kube-system --from-literal=username=${var.bigip_username} --from-literal=password=${var.bigip_password}",
-      "kubectl create serviceaccount bigip-ctlr -n kube-system",
-      "kubectl create clusterrolebinding k8s-bigip-ctlr-clusteradmin --clusterrole=cluster-admin --serviceaccount=kube-system:k8s-bigip-ctlr",
-      "kubectl create -f /home/ubuntu/cismanifests/bigip-ctlr-clusterrole.yaml",
-      "kubectl create -f /home/ubuntu/cismanifests/cis-customresourcedefinition.yaml",
       "helm repo add f5-stable https://f5networks.github.io/charts/stable",
       "helm install -f /home/ubuntu/cismanifests/value.yaml f5-stable/f5-bigip-ctlr --generate-name",
       "kubectl create -f /home/ubuntu/cismanifests/bigip-node.yaml"
